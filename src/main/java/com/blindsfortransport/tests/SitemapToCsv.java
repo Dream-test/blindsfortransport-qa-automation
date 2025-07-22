e95642fd60c8716a809fa0d6d68028df73a951e2
anastasiya-stsiapkova <68887208+anastasiya-stsiapkova@users.noreply.github.com>
2025-07-22 18:17:23 +0200

    Refactoring

--- src/main/java/com/blindsfortransport/tests/SitemapToCsv.java
+++ src/main/java/com/blindsfortransport/tests/SitemapToCsv.java
@@ -21,69 +21,16 @@ public class SitemapToCsv {
         logger.info("SitemapIndexUrl: {}", sitemapIndexUrl);
         Set<String> allUrls = new HashSet<>();
 
-        try { // try-catch to handle IOException at the top level
-            //Download all nested sitemap's
-            Document indexDoc = Jsoup.connect(sitemapIndexUrl).get();
+        try { 
+            // try-catch to handle IOException at the top level
+            // Download all nested sitemap's
+            Elements sitemapLinks = Jsoup
+                                    .connect(sitemapIndexUrl)
+                                    .get()
+                                    .select("sitemap > loc");
 
-            Elements sitemapLinks = indexDoc.select("sitemap > loc");
-
-            // Handle the case where sitemapIndexUrl might be a direct sitemap, not an index
-            if (sitemapLinks.isEmpty()) {
-                logger.warn("No nested sitemaps found in sitemap_index.xml using selector 'sitemap > loc'. Attempting to parse as a direct sitemap.");
-                logger.info("Processing nested SitemapIndex how Sitemap Url: {}", sitemapIndexUrl);
-
-                /*
-                Elements pageLinks = indexDoc.select("url > loc");
-                for (var pageLoc : pageLinks) {
-                    allUrls.add(pageLoc.text());
-                }
-                 */
-
-                extractUrls(indexDoc, allUrls);
-            } else {
-                for (var sitemap : sitemapLinks) {
-                    String sitemapUrl = sitemap.text();
-                    logger.info("Processing nested Sitemap Url: {}", sitemapUrl);
-
-                    try { // try-catch to handle errors when loading nested sitemaps
-                        Document sitemapDoc = Jsoup.connect(sitemapUrl).get();
-
-                        /*
-                        Elements pageLinks = sitemapDoc.select("url > loc");
-
-                        for (var page : pageLinks) {
-                            String url = page.text();
-                            // check for the domain to make sure they are the right URLs
-                            if (url.startsWith("https://www.blindsfortransport.com/")) {
-                                logger.info("Add Url: {}", url);
-                                allUrls.add(url);
-                            }
-                        }
-                         */
-
-                        extractUrls(sitemapDoc, allUrls);
-                    } catch (IOException e) {
-                        logger.error("Error downloading or parsing sitemap {}: {}", sitemapUrl, e.getMessage());
-                    }
-                }
-            }
-
-
-                try (FileWriter urlWriter = new FileWriter(AppConfig.URLS_CSV_PATH);
-                     FileWriter urlAndStatusWriter = new FileWriter(AppConfig.URLS_WITH_STATUS_CSV_PATH)) {
-
-                    urlWriter.write("url\n");
-                    urlAndStatusWriter.write("url,expected_status\n");
-
-                    if (allUrls.isEmpty()) {
-                        logger.warn("No URLs were extracted from the sitemap. Files will still be created but will be empty.");
-                    } else {
-                    for (String url : allUrls) {
-                        urlWriter.write(url + "\n");
-                        urlAndStatusWriter.write(url + ",200\n");
-                    }
-                }
-            }
+            allUrls = processElements(sitemapLinks);
+            writeToCsv(allUrls);
 
             logger.info("Collected {} unique URLs from sitemap(s).", allUrls.size());
             logger.info("""
@@ -95,14 +42,42 @@ public class SitemapToCsv {
                     AppConfig.URLS_CSV_PATH,
                     AppConfig.URLS_WITH_STATUS_CSV_PATH,
                     allUrls.size());
-            //logger.info("CSV-file 'urls.csv' was created successfully. It contains {} URLs.", allUrls.size());
         } catch (IOException e) {
             logger.error("Error downloading or parsing sitemap index {}: {}", sitemapIndexUrl, e.getMessage());
         }
     }
 
-    private static void  extractUrls(Document sitemapDoc, Set<String> allUrls) {
+    private static Set<String> processElements(Elements sitemapLinks) {
+        // Handle the case where sitemapIndexUrl might be a direct sitemap, not an index
+        if (sitemapLinks.isEmpty()) {
+            logger.warn("No nested sitemaps found in sitemap_index.xml using selector 'sitemap > loc'. Attempting to parse as a direct sitemap.");
+            logger.info("Processing nested SitemapIndex how Sitemap Url: {}", sitemapIndexUrl);
+
+            return extractUrls(indexDoc, allUrls);
+        }
+        
+        return processSitemapLinks(sitemapLinks);
+    }
+
+    private static Set<String> processSitemapLinks(Elements sitemapLinks) {
+        for (var sitemap : sitemapLinks) {
+            String sitemapUrl = sitemap.text();
+            logger.info("Processing nested Sitemap Url: {}", sitemapUrl);
+
+            try { // try-catch to handle errors when loading nested sitemaps
+                Document sitemapDoc = Jsoup.connect(sitemapUrl).get();
+
+                return extractUrls(sitemapDoc, allUrls);
+            } catch (IOException e) {
+                logger.error("Error downloading or parsing sitemap {}: {}", sitemapUrl, e.getMessage());
+            }
+        }
+    }
+
+    private static Set<String>  extractUrls(Document sitemapDoc) {
         Elements pageLinks = sitemapDoc.select("url > loc");
+        Set<String> allUrls = new HashSet<>();
+
         for (var page : pageLinks) {
             String url = page.text();
             // check for the domain to make sure they are the right URLs
@@ -111,5 +86,25 @@ public class SitemapToCsv {
                 allUrls.add(url);
             }
         }
+
+        return allUrls;
+    }
+
+    private void writeToCsv(Set<String> allUrls) {
+        try (FileWriter urlWriter = new FileWriter(AppConfig.URLS_CSV_PATH);
+            FileWriter urlAndStatusWriter = new FileWriter(AppConfig.URLS_WITH_STATUS_CSV_PATH)) {
+                urlWriter.write("url\n");
+                urlAndStatusWriter.write("url,expected_status\n");
+
+                if (allUrls.isEmpty()) {
+                    logger.warn("No URLs were extracted from the sitemap. Files will still be created but will be empty.");
+                    return;
+                }
+                
+                for (String url : allUrls) {
+                    urlWriter.write(url + "\n");
+                    urlAndStatusWriter.write(url + ",200\n");
+                }
+            }
     }
 }
